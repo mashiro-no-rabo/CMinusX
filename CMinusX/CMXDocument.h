631f405b69b9c78f3cf9f9b53d36f5d90ecac620
AquarHEAD L <aquarhead@gmail.com>
2013-06-08 03:58:56 +0800

    Initial Commit

---
+++ CMinusX/CMXDocument.h
@@ -0,0 +1,13 @@
+//
+//  CMXDocument.h
+//  CMinusX
+//
+//  Created by AquarHEAD L. on 6/8/13.
+//  Copyright (c) 2013 Team.TeaWhen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CMXDocument : NSDocument
+
+@end
