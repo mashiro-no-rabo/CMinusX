42b4fd552b3bc1c16ba41e041d218cfa9216d50e
AquarHEAD L <aquarhead@gmail.com>
2013-06-28 21:58:56 +0800

    done matchArg

--- CMinusX/CMinusCompiler/CMParser.m
+++ CMinusX/CMinusCompiler/CMParser.m
@@ -44,7 +44,6 @@
     return prog;
 }
 
-
 - (STDeclarationNode *)matchDecl {
     STDeclarationNode *decl = [STDeclarationNode new];
     decl.info = [NSMutableDictionary new];
@@ -58,14 +57,25 @@
                 // int variable
                 [self.symtab insertSymbolName:[idtk.info objectForKey:@"id"] withInfo:@{@"type": @"int"}];
                 decl.type = STDeclVar;
-                decl.info = [@{@"id": [idtk.info objectForKey:@"id"], @"type": @"int"} mutableCopy];
+                [decl.info setObject:[idtk.info objectForKey:@"id"] forKey:@"id"];
+                [decl.info setObject:@"int" forKey:@"type"];
             }
             else if (typtk.type == TokenArrayLeft) {
                 // array variable
                 CMToken *sizetk = [self nextToken];
+                if (sizetk.type != TokenNUM) {
+                    self.tkpos = oripos;
+                    return nil;
+                }
                 [self.symtab insertSymbolName:[idtk.info objectForKey:@"id"] withInfo:@{@"type": @"array", @"size": [sizetk.info objectForKey:@"value"]}];
                 decl.type = STDeclVar;
-                decl.info = [@{@"id": [idtk.info objectForKey:@"id"], @"type": @"int", @"size": [sizetk.info objectForKey:@"value"]} mutableCopy];
+                [decl.info setObject:[idtk.info objectForKey:@"id"] forKey:@"id"];
+                [decl.info setObject:@"array" forKey:@"type"];
+                [decl.info setObject:[sizetk.info objectForKey:@"value"] forKey:@"size"];
+                if ([self nextToken].type != TokenArrayRight) {
+                    self.tkpos = oripos;
+                    return nil;
+                }
             }
             else if (typtk.type == TokenArgsLeft) {
                 // function return int
@@ -83,20 +93,69 @@
             }
         }
         else {
+            self.tkpos = oripos;
             return nil;
         }
     }
     else if (tk.type == TokenVoid) {
         CMToken *idtk = [self nextToken];
+        [self.symtab pushLevel];
+        NSMutableArray *args = [NSMutableArray new];
+        STArgumentNode *arg = [self matchArg];
+        [args addObject:arg];
+        while ([self nextToken].type == TokenComma) {
+            arg = [self matchArg];
+            [args addObject:arg];
+        }
+        [self.symtab popLevel];
+        decl.type = STDeclFunc;
     }
     else {
         self.tkpos = oripos;
         return nil;
     }
+    self.tkpos += 1;
+    return decl;
 }
 
 - (STArgumentNode *)matchArg {
-    
+    STArgumentNode *arg = [STArgumentNode new];
+    arg.info = [NSMutableDictionary new];
+    int oripos = self.tkpos;
+    CMToken *tk = [self nextToken];
+    if (tk.type == TokenInt) {
+        CMToken *idtk = [self nextToken];
+        if (idtk.type == TokenID) {
+            CMToken *typtk = [self nextToken];
+            if ((typtk.type == TokenComma) || (typtk.type == TokenArgsRight)) {
+                [arg.info setObject:[idtk.info objectForKey:@"id"] forKey:@"id"];
+                [arg.info setObject:@"int" forKey:@"type"];
+            }
+            else if (typtk.type == TokenArrayLeft) {
+                CMToken *sizetk = [self nextToken];
+                if (sizetk.type != TokenNUM) {
+                    self.tkpos = oripos;
+                    return nil;
+                }
+                [arg.info setObject:[idtk.info objectForKey:@"id"] forKey:@"id"];
+                [arg.info setObject:@"array" forKey:@"type"];
+                [arg.info setObject:[sizetk.info objectForKey:@"value"] forKey:@"size"];
+                if ([self nextToken].type != TokenArrayRight) {
+                    self.tkpos = oripos;
+                    return nil;
+                }
+            }
+        }
+        else {
+            self.tkpos = oripos;
+            return nil;
+        }
+    }
+    else {
+        self.tkpos = oripos;
+        return nil;
+    }
+    return arg;
 }
 
 - (CMToken *)nextToken {
